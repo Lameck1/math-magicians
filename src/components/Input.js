649fbaae032ab49b821d9f17589d24f094f9a770
Lameck1 <lameck72@gmail.com>
2021-08-11 04:49:57 +0300

    Refactor Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,25 +1,51 @@
-const Input = () => (
-  <div className="buttons-grid">
-    <button data="AC" className="input-btn" type="button">AC</button>
-    <button data="+/-" className="input-btn" type="button">+/-</button>
-    <button data="%" className="input-btn" type="button">%</button>
-    <button data="÷" className="input-btn operator" type="button">÷</button>
-    <button data="7" className="input-btn" type="button">7</button>
-    <button data="8" className="input-btn" type="button">8</button>
-    <button data="9" className="input-btn" type="button">9</button>
-    <button data="x" className="input-btn operator" type="button">x</button>
-    <button data="4" className="input-btn" type="button">4</button>
-    <button data="5" className="input-btn" type="button">5</button>
-    <button data="6" className="input-btn" type="button">6</button>
-    <button data="-" className="input-btn operator" type="button">-</button>
-    <button data="1" className="input-btn" type="button">1</button>
-    <button data="2" className="input-btn" type="button">2</button>
-    <button data="3" className="input-btn" type="button">3</button>
-    <button data="+" className="input-btn operator" type="button">+</button>
-    <button data="0" className="input-btn col-span-2" type="button">0</button>
-    <button data="." className="input-btn" type="button">.</button>
-    <button data="=" className="input-btn operator" type="button">=</button>
-  </div>
-);
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { clickHandler } = this.props;
+    clickHandler(buttonName);
+  }
+
+  render() {
+    return (
+      <div className="buttons-grid">
+        <Button clickHandler={this.handleClick} name="AC" />
+        <Button clickHandler={this.handleClick} name="+/-" />
+        <Button clickHandler={this.handleClick} name="%" />
+        <Button clickHandler={this.handleClick} operator name="÷" />
+        <Button clickHandler={this.handleClick} name="7" />
+        <Button clickHandler={this.handleClick} name="8" />
+        <Button clickHandler={this.handleClick} name="9" />
+        <Button clickHandler={this.handleClick} operator name="x" />
+        <Button clickHandler={this.handleClick} name="4" />
+        <Button clickHandler={this.handleClick} name="5" />
+        <Button clickHandler={this.handleClick} name="6" />
+        <Button clickHandler={this.handleClick} operator name="-" />
+        <Button clickHandler={this.handleClick} name="1" />
+        <Button clickHandler={this.handleClick} name="2" />
+        <Button clickHandler={this.handleClick} name="3" />
+        <Button clickHandler={this.handleClick} operator name="+" />
+        <Button clickHandler={this.handleClick} spanning name="0" />
+        <Button clickHandler={this.handleClick} name="." />
+        <Button clickHandler={this.handleClick} operator name="=" />
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  clickHandler: PropTypes.func,
+};
+
+Input.defaultProps = {
+  clickHandler: null,
+};
 
 export default Input;
