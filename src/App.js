73c49aae9e53802202c00831e6acd1e6728be475
Lameck1 <lameck72@gmail.com>
2021-08-19 04:43:20 +0300

    Implement routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import homeData from './data/homeData';
+import QUOTE from './data/quoteData';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,15 +19,44 @@ export default class App extends React.Component {
   }
 
   render() {
+    const { title, text } = homeData;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Math Magicians
-          </h1>
-        </header>
-        <Calculator />
-      </div>
+      <Router>
+        <main className="App d-flex">
+          <header className="d-flex">
+            <div>
+              <span>
+                <Link to="/" className="logo">Math Magicians</Link>
+              </span>
+            </div>
+            <nav>
+              <ul className="nav-links d-flex">
+                <li>
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li>
+                  <Link to="/calculator" className="nav-link">Calculator</Link>
+                </li>
+                <li>
+                  <Link to="/quote" className="nav-link">Quote</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <Switch>
+            <Route path="/calculator">
+              <Calculator />
+            </Route>
+            <Route path="/quote">
+              <Quote quote={QUOTE} />
+            </Route>
+            <Route path="/">
+              <Home title={title} text={text} />
+            </Route>
+          </Switch>
+          <Footer />
+        </main>
+      </Router>
     );
   }
 }
