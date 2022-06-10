868378f7a2b8fd2aa98a48cdf3f67420fe803142
Lameck1 <lameck72@gmail.com>
2022-06-10 17:21:01 +0300

    Refactor react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
+// eslint-disable-next-line object-curly-newline
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -21,42 +17,44 @@ export default class App extends React.Component {
   render() {
     const { title, text } = homeData;
     return (
-      <Router>
+      <BrowserRouter>
         <main className="App d-flex">
           <header className="d-flex">
             <div>
               <span>
-                <Link to="/" className="logo">Math Magicians</Link>
+                <Link to="/" className="logo">
+                  Math Magicians
+                </Link>
               </span>
             </div>
             <nav>
               <ul className="nav-links d-flex">
                 <li>
-                  <Link to="/" className="nav-link">Home</Link>
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/calculator" className="nav-link">Calculator</Link>
+                  <Link to="/calculator" className="nav-link">
+                    Calculator
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/quote" className="nav-link">Quote</Link>
+                  <Link to="/quote" className="nav-link">
+                    Quote
+                  </Link>
                 </li>
               </ul>
             </nav>
           </header>
-          <Switch>
-            <Route path="/calculator">
-              <Calculator />
-            </Route>
-            <Route path="/quote">
-              <Quote quote={QUOTE} />
-            </Route>
-            <Route path="/">
-              <Home title={title} text={text} />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quote" element={<Quote quote={QUOTE} />} />
+            <Route path="/" element={<Home title={title} text={text} />} />
+          </Routes>
           <Footer />
         </main>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
