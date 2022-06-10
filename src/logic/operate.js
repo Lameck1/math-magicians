c54ef991b34707aa18903ccb6e0b7d4e2adb5d3f
Lameck1 <lameck72@gmail.com>
2022-06-10 17:22:16 +0300

    Add modulus operation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,7 +2,9 @@ import Big from 'big.js';
 
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne || '0');
-  const two = Big(numberTwo || (operation === '÷' || operation === 'x' ? '1' : '0')); // If dividing or multiplying, then 1 maintains current value in cases of null
+  const two = Big(
+    numberTwo || (operation === '÷' || operation === 'x' ? '1' : '0'),
+  ); // If dividing or multiplying, then 1 maintains current value in cases of null
   if (operation === '+') {
     return one.plus(two).toString();
   }
@@ -15,6 +17,13 @@ export default function operate(numberOne, numberTwo, operation) {
   if (operation === '÷') {
     try {
       return one.div(two).toString();
+    } catch (err) {
+      return err.message;
+    }
+  }
+  if (operation === '%') {
+    try {
+      return one.mod(two).toString();
     } catch (err) {
       return err.message;
     }
